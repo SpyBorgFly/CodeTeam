6f4f6f4269515ebe514662c0c540636635146493
SpyBorgFly <zxc.inside@gmail.com>
2024-10-16 17:18:57 +0300

    Добавлены дополнительные файлы css и создано приложение accounts для регистрации

--- laurent/urls.py
+++ laurent/urls.py
@@ -5,6 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('projects/', include('projects.urls'))
-
+    path('projects/', include('projects.urls')),
+    path('user/', include('accounts.urls'))
 ]
