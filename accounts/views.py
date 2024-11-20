767b8c2bfb6d25cbab423f55799e143f1d85ca11
4315 Бабайкин Н.  П. <zxc.inside@gmail.com>
2024-11-20 22:50:11 +0300

    испрален баг при котором показывалась аватарка пользователя, чей профиль просматривается, а не того, кто просматривает, также исправлен баг с редактированием чужого профиля

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from .forms import CustomUserCreationForm, UserProfileForm, UserSettingsForm
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import UserProfile
+from django.http import Http404
 
 class RegisterView(View):
     def get(self, request):
@@ -69,12 +70,16 @@ class ProfileView(View):
 class EditProfileView(View):
     def get(self, request, username):
         user = get_object_or_404(User, username=username)
+        if request.user != user:
+            raise Http404("Вы не можете редактировать профиль другого пользователя.")
         user_profile = user.userprofile
         form = UserProfileForm(instance=user_profile)
         return render(request, 'accounts/edit_profile.html', {'form': form, 'user_profile': user_profile, 'user': user})
 
     def post(self, request, username):
         user = get_object_or_404(User, username=username)
+        if request.user != user:
+            raise Http404("Вы не можете редактировать профиль другого пользователя.")
         user_profile = user.userprofile
         form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
         if form.is_valid():
